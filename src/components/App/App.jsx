d4091d51107114f2f2c6d5a72dee04fd4bf681cf
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-02 11:26:03 +0100

    save-change

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,33 @@
 import UseMemoExample from '../UseMemoExample'
+import RefExample from '../RefExample'
+import { useRef, useState } from 'react';
 
 export default function App() {
+  //Ref це не реактивне значення, зміна Ref не впливає на оновлення компоненту, тобто не форсить рендер, це не probs i не useState.
+  // const clicks = useRef();
+  const [clicks, setClicks] = useState(0);
+  const date = new Date();
+
+  const handelCLick = () => {
+
+  };
+
+  //Це приклад відноситься до Ref
+  // const handleClick = () => {
+  //   clicks.current += 1;
+  //   console.log(handleClick);
+  // }
   return (
-    < UseMemoExample />
-  )
+    <>
+      <div>
+        <button onClick={handelCLick}>{clicks.current}</button>
+        <p>Date: {date.toLocaleTimeString}</p>
+      </div>    
+      <hr />
+      < UseMemoExample />
+      <hr />
+      < RefExample />
+    </>
+  );
 }
 
